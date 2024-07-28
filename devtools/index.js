ce788abcd1a0876837f19dc2786cc0658a52a907
Ian Wijma <ian@wij.ma>
2024-07-28 20:36:17 +1000

    Made the network tab work

--- devtools/index.js
+++ devtools/index.js
@@ -1,23 +1,3 @@
-const addNetworkListener = async () => {
-	const handlePage = (redirect) => {
-		console.log('page', { redirect });
-	};
-	const handleNetwork = (event) => {
-		const { request = {} } = event;
-		const { url = '', method = '', headers = [] } = request;
-
-		const filterHeader = ({ name }) =>
-			name === 'x-team-identification-extension';
-		const [wantedHeader = {}] = headers.filter(filterHeader);
-		const { value: team = '' } = wantedHeader;
-
-		console.log('network', { url, method, team });
-	};
-
-	chrome.devtools.network.onNavigated.addListener(handlePage);
-	chrome.devtools.network.onRequestFinished.addListener(handleNetwork);
-};
-
 const addNetworkPanel = async () => {
 	chrome.devtools.panels.create(
 		'Team Identification Extension',
@@ -32,7 +12,6 @@ const addNetworkPanel = async () => {
 const main = async () => {
 	console.log('Hello Devtools!');
 
-	await addNetworkListener();
 	await addNetworkPanel();
 };
 
--- devtools/network/index.js
+++ devtools/network/index.js
@@ -1,5 +1,73 @@
+const tableBody = document.getElementById('network-table');
+
+let rowCount = 0;
+
+const resetBody = () => {
+	tableBody.innerHTML = '';
+	rowCount = 0;
+};
+
+const addPage = ({ redirect }) => {
+	const row = document.createElement('tr');
+	row.style.background = 'gray';
+
+	const column = document.createElement('td');
+	column.colSpan = 3;
+	column.textContent = `Redirect: ${redirect}`;
+
+	row.appendChild(column);
+	tableBody.prepend(row);
+};
+
+const addNetwork = ({ url, method, team }) => {
+	const urlObj = new URL(url);
+
+	const row = document.createElement('tr');
+	row.style.background = rowCount % 2 === 0 ? 'lightgray' : '';
+
+	const pathElement = document.createElement('td');
+	pathElement.textContent = String(urlObj.pathname);
+	row.appendChild(pathElement);
+
+	const methodElement = document.createElement('td');
+	methodElement.textContent = String(method);
+	row.appendChild(methodElement);
+
+	const teamElement = document.createElement('td');
+	teamElement.textContent = String(team);
+	row.appendChild(teamElement);
+
+	tableBody.prepend(row);
+	rowCount++;
+};
+
+const addNetworkListener = async () => {
+	const handlePage = (redirect) => {
+		console.log('page', { redirect });
+		// addPage({ redirect });
+		resetBody();
+	};
+	const handleNetwork = (event) => {
+		const { request = {} } = event;
+		const { url = '', method = '', headers = [] } = request;
+
+		const filterHeader = ({ name }) =>
+			name === 'x-team-identification-extension';
+		const [wantedHeader = {}] = headers.filter(filterHeader);
+		const { value: team = '' } = wantedHeader;
+
+		console.log('network', { url, method, team });
+		addNetwork({ url, method, team });
+	};
+
+	chrome.devtools.network.onNavigated.addListener(handlePage);
+	chrome.devtools.network.onRequestFinished.addListener(handleNetwork);
+};
+
 const main = async () => {
 	console.log('Hello Network!');
+
+	await addNetworkListener();
 };
 
 main();
